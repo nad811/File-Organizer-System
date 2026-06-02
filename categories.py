6b712ed3d8a3b0ca912668a82582cd16c0efedea
nad811 <nad349393@gmail.com>
2026-06-02 14:25:44 +0530

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,20 @@
+IMAGE_FILES = [
+    ".jpg",
+    ".jpeg",
+    ".png"
+]
+
+DOCUMENT_FILES = [
+    ".doc",
+    ".docx",
+    ".txt"
+]
+
+PDF_FILES = [
+    ".pdf"
+]
+
+VIDEO_FILES = [
+    ".mp4",
+    ".avi"
+]
